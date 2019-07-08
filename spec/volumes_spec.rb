74e4dbbf5d5b2a609b1f77b7de32f415eae6e86f
Hilary Berigan <berigan.hilary@gmail.com>
2019-07-08 17:00:39 -0500

    last of tests

--- spec/volumes_spec.rb
+++ spec/volumes_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 require_relative '../lib/books_api'
 require_relative '../lib/volume'
+require 'faker'
 
 describe "Books API List Request" do
   it "returns a Response Object that contains a list of Volumes" do 
@@ -8,7 +9,7 @@ describe "Books API List Request" do
   		        start_page: 1 }
     
   	books_api = BooksApi.new(options)
-  	response = books_api.list('hilary')
+  	response = books_api.list(Faker::Name.name)
     expect(response).to be_an_instance_of(Books::Response)
     expect(response.volumes).to be_an_instance_of(Array)
     expect(response.volumes.first).to be_an_instance_of(Books::Volume)
